7d4bb229b3bfd1fc9028b2e3c034f92d597e9838
max101g <maxgithinji101@gmail.com>
2023-03-24 17:28:27 +0300

    commit

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,6 +2,7 @@ class PowersController < ApplicationController
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    rescue_from ActionController::UnpermittedParameters, with: :handle_errors
 
     def index
         powers = Power.all 
@@ -35,6 +36,10 @@ class PowersController < ApplicationController
 
     def render_unprocessable_entity_response(invalid)
         render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
+
+    def handle_errors
+        render json: { "Unpermitted Parameters": params.to_unsafe_h.except(:controller, :action, :name, :description)}, status: :unprocessable_entity 
     end
 
 end
