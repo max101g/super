d0af78d8dedc674e1943645d374c7cf03583e132
max101g <maxgithinji101@gmail.com>
2023-03-25 09:43:52 +0300

    commit

--- app/controllers/heropowers_controller.rb
+++ app/controllers/heropowers_controller.rb
@@ -4,4 +4,15 @@ class HeropowersController < ApplicationController
         heropowers = Heropower.all 
         render json: heropowers
     end 
+
+    def create
+        heropower = Heropower.create!(heropower_params)
+        render json: heropower, status: :created
+    end
+
+    private
+
+    def heropower_params
+        params.permit(:strength, :hero_id, :power_id)
+    end
 end
--- app/models/heropower.rb
+++ app/models/heropower.rb
@@ -1,4 +1,6 @@
 class Heropower < ApplicationRecord
   belongs_to :hero
   belongs_to :power
+
+  validates :strength, presence: true, inclusion: { in: ['Average', 'Strong', 'Weak'] }
 end
